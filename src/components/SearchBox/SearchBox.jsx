1907e4e08b9cb57c1988993d07f3701af9a3c94c
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-04-19 23:44:48 +0300

    Initial commit

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,13 @@
+
+import css from "./SearchBox.module.css";
+
+const SearchBox = ({ filter, onChangeFilter }) => {
+  return (
+    <label className={css.label}>
+      Finde contacts by name
+      <input placeholder="Search..." value={filter} onChange={onChangeFilter} />
+    </label>
+  );
+};
+
+export default SearchBox;
