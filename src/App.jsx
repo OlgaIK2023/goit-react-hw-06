c5b77555235fb99fa10a7d9a3ac6e3db88c6aa1d
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-04-21 16:39:35 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import { nanoid } from "nanoid";
 import { useSelector, useDispatch } from "react-redux";
+import {
+  deleteContact,
+  setFilter,
+} from "./redux/contactlist/contactlistReducer";
 
 // const initialContacts = [
 //   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
@@ -13,7 +17,6 @@ import { useSelector, useDispatch } from "react-redux";
 //   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
 //   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
 // ];
-
 
 //APP code below
 
@@ -37,40 +40,32 @@ function App() {
   const contacts = useSelector((state) => state.contactList.contacts);
   const filter = useSelector((state) => state.contactList.filter);
 
-    const addContact = (newContact) => {
-    const finalContact = { 
+  const addContact = (newContact) => {
+    const finalContact = {
       ...newContact,
-       id: nanoid(),
-       };
+      id: nanoid(),
+    };
 
-    const action = {type: "contactlist/addContact", payload: finalContact };
+    const action = addContact(finalContact);
 
     dispatch(action);
-
-    // setContacts((prevContacts) => [...prevContacts, finalContact]);
-
-      };
-
-      
+  };
 
   const filteredContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 
   const onDeleteContact = (contactId) => {
-    const action = {type: "contactlist/deleteContact", payload: contactId };
-    dispatch(action);
+    const action = deleteContact(contactId);
 
-    // setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== contactId));
+    dispatch(action);
   };
 
   const onChangeFilter = (event) => {
-    const action = {type: "contactlist/setFilter", payload: event.target.value };
+    const action = setFilter(event.target.value);
+
     dispatch(action);
-
-    // setFilter(event.target.value);
   };
-
 
   return (
     <div>
--- src/redux/contactlist/contactlistReducer.js
+++ src/redux/contactlist/contactlistReducer.js
@@ -1,30 +1,59 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const INITIAL_STATE = {
   contacts: [],
   filter: "",
 };
 
-export const contactlistReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case "contactlist/addContact": {
-        return {
-            ...state,
-            contacts: [...state.contacts, action.payload],
-        }
-    }
-    case "contactlist/deleteContact": {
-        return {
-            ...state,
-            contacts: state.contacts.filter ((contact ) => contact.id !== action.payload)
-        }
-    }
-    case "contactlist/setFilter": {
-        return {
-            ...state,
-            filter: action.payload
-        }
-    }
-    default:
-      return state;
-  }
-};
+// export const contactlistReducer = (state = INITIAL_STATE, action) => {
+//   switch (action.type) {
+//     case "contactlist/addContact": {
+//         return {
+//             ...state,
+//             contacts: [...state.contacts, action.payload],
+//         }
+//     }
+//     case "contactlist/deleteContact": {
+//         return {
+//             ...state,
+//             contacts: state.contacts.filter ((contact ) => contact.id !== action.payload)
+//         }
+//     }
+//     case "contactlist/setFilter": {
+//         return {
+//             ...state,
+//             filter: action.payload
+//         }
+//     }
+//     default:
+//       return state;
+//   }
+// };
+
+
+
+const contactsSlice = createSlice({
+  // Ім'я слайсу
+  name: "contacts",
+  // Початковий стан редюсера слайсу
+  initialState: INITIAL_STATE,
+  // Об'єкт редюсерів
+  reducers: {
+    addContact(state, action) {
+        state.users.push(action.payload);
+    },
+    deleteContact(state, action) {
+        state.users = state.users.filter ((user) => user.id !== action.payload);
+    },
+    setFilter(state, action) {
+        state.filter = action.payload;
+    },
+  },
+});
+
+// Генератори екшенів
+export const {addContact, deleteContact, setFilter } = contactsSlice.actions;
+
+// Редюсер слайсу
+export const contactlistReducer = contactsSlice.reducer;
 
