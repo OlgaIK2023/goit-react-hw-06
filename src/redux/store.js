82ff2912298bd17a20d26b22eccf8cd098899e12
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-04-21 15:57:55 +0300

    upd

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,9 @@
-import { combineReducers, createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
+import { configureStore } from "@reduxjs/toolkit";
+
 import { contactlistReducer } from "./contactlist/contactlistReducer";
 
-
-const rootReducer = combineReducers(
-    {
-        contactList: contactlistReducer,
-    }
-);
-
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+  reducer: {
+    contactList: contactlistReducer,
+  },
+});
