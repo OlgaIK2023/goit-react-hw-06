a1658ae66ce4d161df367a5893fd904d2141ede1
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-04-22 15:00:07 +0300

    upd persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+
 import { contactlistReducer } from "./contactlistReducer";
+
+
+const contactlistPeristConfig = {
+    key: "contactlist",
+    storage,
+    whitelist: ["contacts"],
+  };
+
 
 export const store = configureStore({
   reducer: {
-    contactList: contactlistReducer,
+    contactList: persistReducer (contactlistPeristConfig, contactlistReducer),
   },
+
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+
 });
+
+export const persistor = persistStore(store);
